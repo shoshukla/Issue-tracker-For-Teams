e9e925e2007c6b5932f023c8a055df0b9ac9b591
shoshukla <shobhitshukla906@gmail.com>
2022-09-13 01:49:29 -0700

    Issue tracker

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Login = () => {
+
+  const loginSubmit = (formdata)=>{
+    console.log(Formdata);
+  }
+  return (
+<section className='vh-100' style={{backgroundColor:"ghostwhite"}}>
+<div className='container py-5 h-100'>
+  <div className='row d-flex justify-content-center align-items-center h-100'>
+    <div></div>
+  </div>
+</div>
+</section>
+  )
+}
+
+export default 
